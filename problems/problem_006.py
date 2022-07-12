7b9b844746d1a78e3cea6f021dbc1b1a4059ec22
Jay <jayandrego@gmail.com>
2022-07-11 17:18:41 -0700

    Updated problem 006

--- problems/problem_006.py
+++ problems/problem_006.py
@@ -10,4 +10,23 @@
 # problem to get a good feel for how to solve it.
 
 def can_skydive(age, has_consent_form):
-    pass
+    if has_consent_form == 'yes':
+        return True
+
+
+
+# two arguments to pass
+
+# def function - takes two parameters
+
+# if persons_form == b -> consent form
+# elif persons_age > = 18
+# else ->
+
+# return message True/False and/or "You can skydive", "you can't skydive" -> covering specific conditions
+
+skydiver_age_check = input("Are you older than 17? (yes/no) ")  # yes / no
+consent_form = input("Do you have a consent form? (yes/no) ")  # yes / no
+
+# call the function and test it
+print(can_skydive(skydiver_age_check, consent_form))
