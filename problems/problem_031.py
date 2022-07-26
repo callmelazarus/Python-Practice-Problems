31c217bb5c604d407b8f8ce90b4f8eea1df4ce86
Jay <jayandrego@gmail.com>
2022-07-25 22:28:01 -0700

    finished up to problem 34

--- problems/problem_031.py
+++ problems/problem_031.py
@@ -13,5 +13,23 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+
 def sum_of_squares(values):
-    pass
+    if values == []:
+        return None
+    final = 0
+    for num in values:
+        final += num*num
+    return final
+
+
+
+        # returns the numbers squared in a list
+        # how do you square a number?
+        # will need to loop, taking each value and squaring it
+
+list1 = [1, 2, 3] # returns 1*1+2*2+3*3=14
+list2 = [-1, 0, 1] #returns (-1)*(-1)+0*0+1*1=2
+
+print(sum_of_squares(list1))
+print(sum_of_squares(list2))
